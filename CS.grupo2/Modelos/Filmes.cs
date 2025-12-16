43e8277957b506ddd50025dcaf332e875094ecf5
Kauane Barbosa <kauanebarbosa@hotmail.com>
2025-12-16 19:57:57 -0300

    Pasta filmes

---
+++ CS.grupo2/Modelos/Filmes.cs
@@ -0,0 +1,10 @@
+namespace CineScroll.GRUPO2.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public int Ano { get; set; }
+    public double Avaliacao { get; set; }
+}
